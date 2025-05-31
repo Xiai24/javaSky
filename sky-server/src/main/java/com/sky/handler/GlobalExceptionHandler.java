3c90d0cd8bbfde921046d0bcf21427e6daaad333
Liting <hx34879@163.com>
2025-05-31 15:47:56 +0800

    新增菜品业务添加

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -11,6 +11,8 @@ import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
+ * @RestControllerAdvice 注解标识，能捕获项目中 Controller 层抛出的各类异常，
+ * 并利用类中被 @ExceptionHandler 注解的方法对不同类型异常进行针对性处理，如记录异常日志、返回合适的错误信息给前端等
  */
 @RestControllerAdvice
 @Slf4j
@@ -27,8 +29,14 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理sql异常
+     * @param ex
+     * @return
+     */
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //当用户名已存在的时候
         String message = ex.getMessage();
         if (message.contains(("Duplicate entry"))) {
             String[] split = message.split(" ");
